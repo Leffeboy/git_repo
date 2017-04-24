b4ea7ab6fc07a6dbc2e753eee4041fe020718231
Ahmadi Laith <Laith.Ahmadi@afconsult.com>
2017-04-24 13:15:22 +0200

    second commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Summa s= new Summa();
+		int z= s.add(5, 6);
+		System.out.println("Sum " + z);
+	}
+
+}
